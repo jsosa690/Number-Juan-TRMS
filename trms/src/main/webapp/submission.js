fda9347993187463b42928b6a1092164cc8d27ba
Juan Sosa <jsosa690@gmail.com>
2019-03-10 03:05:49 -0500

    updated form

--- trms/src/main/webapp/submission.js
+++ trms/src/main/webapp/submission.js
@@ -1,47 +1,25 @@
-function toJSONString( form ) {
-	var obj = {};
-	var elements = form.querySelectorAll( "input, select" );
-	for( var i = 0; i < elements.length; ++i ) {
-		var element = elements[i];
-		var name = element.name;
-		var value = element.value;
+function toJSONString(name, date, timeStart, timeEnd, loc, desc, amount, sel, sup, ben) {
+	var obj = {
+			fullName : name,
+			startDate : date,
+			startTime : timeStart,
+			endTime : timeEnd,
+			location : loc,
+			description : desc,
+			cost : amount,
+			select : sel,
+			supervisor : sup,
+			benCo : ben
+	};
+	var url = 'http://localhost:8080/trms/form';
+	fetch(url, {
+		  method: 'POST', // or 'PUT'
+		  body: JSON.stringify(obj), // data can be `string` or {object}!
+		  headers:{
+		    'Content-Type': 'application/json'
+		  }
+		}).then(res => res.text())
+		.then(response => console.log('Success:', JSON.stringify(response)))
+		.catch(error => console.error('Error:', error));
 
-		if( name ) {
-			obj[ name ] = value;
-		}
-	}
-
-	return JSON.stringify( obj );
 }
-
-document.addEventListener( "DOMContentLoaded", function() {
-	var form = document.getElementById( "test" );
-	var output = document.getElementById( "output" );
-	form.addEventListener( "submit", function( e ) {
-		e.preventDefault();
-		var url = '/trms/form';
-		var data = {username: 'example'};
-		fetch(url, {
-			  method: 'POST', // or 'PUT'
-			  body: JSON.stringify(data), // data can be `string` or {object}!
-			  headers:{
-			    'Content-Type': 'application/json'
-			  }
-			}).then(res => res.json())
-			.then(response => console.log('Success:', JSON.stringify(response)))
-			.catch(error => console.error('Error:', error));
-//
-//		var data = toJSONString( this );
-//		fetch(url, {
-//			method: 'POST', // or 'PUT'
-//			body: data, // data can be `string` or {object}!
-//			headers:{
-//				'Content-Type': 'application/json'
-//			}
-//		}).then(res => res.json())
-//		.then(response => console.log('Success:', JSON.stringify(response)))
-//		.catch(error => console.error('Error:', error));
-		output.innerHTML = data;
-	})
-});
-
