1e2c6a22dc04160863c09bd46e04fa37c04bfd22
lavoieD <lavoie.davidw@gmail.com>
2019-03-12 22:51:28 -0400

    form propagation complete, DOM manipulation in progress

--- trms/src/main/webapp/populateHomePage.js
+++ trms/src/main/webapp/populateHomePage.js
@@ -1,13 +1,30 @@
 function findSubmissions(){
-	console.log('isathing')
+
 	var url = 'http://localhost:8080/trms/populate';
-    fetch(url, {
-    	method: 'POST',
-    }).then(res => res.text())
+	fetch(url, {
+		method: 'POST',
+	}).then(res => res.text())
 	.then(response => {
-		var fu = JSON.stringify(response);
-		console.log(fu);
-			})
+		console.log(response.length);
+		var forms = JSON.parse(response);
+		forms.forEach(makeFormButton);
+		console.log("there are " + forms.length + " forms");
+
+	})
 	.catch(error => console.error('Error:', error));
-    return fu;
+
+}
+
+function makeFormButton (jsonform) {
+	console.log(jsonform);
+	console.log(jsonform.cost);
+	var newDiv  = document.createElement("div");
+	var newButton = document.createElement("button");
+	var newContent = document.createTextNode("Reimbursement: " + jsonform.formID + " Description: " + jsonform.description); 
+	newButton.appendChild(newContent);
+	newDiv.appendChild(newButton);
+	var currentDiv = document.getElementById("populateReimbursementsButton"); 
+	document.querySelector("body").appendChild(currentDiv);
+
+
 }
