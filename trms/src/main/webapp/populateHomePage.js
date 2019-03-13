da2917818ed066475f0d0946101efe9ca654e449
Juan Sosa <jsosa690@gmail.com>
2019-03-13 18:29:08 -0400

    added form buttons

--- trms/src/main/webapp/populateHomePage.js
+++ trms/src/main/webapp/populateHomePage.js
@@ -1,30 +1,31 @@
 function findSubmissions(){
 
-	var url = 'http://localhost:8080/trms/populate';
-	fetch(url, {
-		method: 'POST',
-	}).then(res => res.text())
-	.then(response => {
-		console.log(response.length);
-		var forms = JSON.parse(response);
-		forms.forEach(makeFormButton);
-		console.log("there are " + forms.length + " forms");
+    var url = 'http://localhost:8080/trms/populate';
+    fetch(url, {
+        method: 'POST',
+    }).then(res => res.text())
+    .then(response => {
+        var forms = JSON.parse(response);
+// If you want to do anything the forms object stores all of the forms to manipulate as you see fit.
+//        N.B. I reccomend using the .forEach() method
+        forms.forEach(makeFormButton);
+        console.log("there are " + forms.length + " forms");
 
-	})
-	.catch(error => console.error('Error:', error));
+    })
+    .catch(error => console.error('Error:', error));
 
 }
 
 function makeFormButton (jsonform) {
-	console.log(jsonform);
-	console.log(jsonform.cost);
-	var newDiv  = document.createElement("div");
-	var newButton = document.createElement("button");
-	var newContent = document.createTextNode("Reimbursement: " + jsonform.formID + " Description: " + jsonform.description); 
-	newButton.appendChild(newContent);
-	newDiv.appendChild(newButton);
-	var currentDiv = document.getElementById("populateReimbursementsButton"); 
-	document.querySelector("body").appendChild(currentDiv);
+    console.log(jsonform);
+    var newDiv  = document.createElement("div");
+    var newButton = document.createElement("button");
+    var newContent = document.createTextNode("Reimbursement: " + jsonform.formID + " Description: " + jsonform.description);
+    newButton.appendChild(newContent);
+    newDiv.appendChild(newButton);
+    sloshy = document.querySelector('.container-fluid')
+    sloshy.insertBefore(newDiv,document.querySelector("#populateReimbursementsButton"))
+    
 
 
 }
